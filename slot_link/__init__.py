d014f8c75fd61e391485298f4132114661fafe85
Mars <mars@squirrel.pub>
2026-08-12 23:01:34 +0200

    moved to codeberg

--- slot_link/__init__.py
+++
@@ -1,7 +0,0 @@
-import bpy
-
-from . import package_key
-
-__all__ = ["register_slot_link", "unregister_slot_link", "package_key"]
-
-register_slot_link, unregister_slot_link = bpy.utils.register_submodule_factory(__name__, ["slot_link", "slot_link_ops", "nla_hackery", "slot_link_ui", "slot_link_ui_parts", "preferences"])
